4e1c81e7e23947563a16c28aa582613b2b734ff2
Kevin Cao <kjcao@proton.me>
2024-03-22 17:02:51 -0400

    Remove old dev server script

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-( cd pub && python3 -m http.server ) &
-entr ./compile.sh "$file" <<<$(cd src/posts; fd)
-
-#while sleep 0.2; do
-#  find * | entr -d sh -c './compile.sh -a && notify-send -t 2000 finished || notify-send -t 2000 -u critical failed'
-#done
